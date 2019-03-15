17f8d1e4e990070995dc466c25a7497505fcedf5
Elliot <aleksandr.gorshkov@tularegion.ru>
2019-03-15 16:41:35 +0300

    hw01_easy done hw01_normal 3-rd task done hw01_hard 1-st and 2-nd task done

--- hw01_normal.py
+++ hw01_normal.py
@@ -23,8 +23,25 @@ a, b = b, a
 print("Пемеременные a=%d" % (a))
 print("Пемеременные b=%d"% (b))
 # Задача-3: Напишите программу, вычисляющую корни квадратного уравнения вида
-# ax² + bx + c = 0.
+# ax² + bx + c = 0.1
 # Коэффициенты уравнения вводятся пользователем.
 # Для вычисления квадратного корня воспользуйтесь функцией sqrt() модуля math:
 # import math
-# math.sqrt(4) - вычисляет корень числа 4
+# matти  тh.sqrt(4) - вычисляет корень числа 4
+import math
+a = int(input("a = "))
+b = int(input("b = "))
+c = int(input("c = "))
+
+discr = b ** 2 - 4 * a * c
+print("Дискриминант D = %.2f" % discr)
+
+if discr > 0:
+    x1 = (-b + math.sqrt(discr)) / (2 * a)
+    x2 = (-b - math.sqrt(discr)) / (2 * a)
+    print("x1 = %.2f \nx2 = %.2f" % (x1, x2))
+elif discr == 0:
+    x = -b / (2 * a)
+    print("x = %.2f" % x)
+else:
+    print("Корней нет")
