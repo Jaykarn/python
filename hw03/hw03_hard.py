dafe944953e29101da26f918f6c6276f5c1323f1
Jaykarn <nightstringer@outlook.com>
2019-05-15 17:37:14 +0300

    hw03 easy 1-st in progress 2-nd done normal 1-st done 2-nd done hard 1-st - did not change

--- hw03/hw03_hard.py
+++ hw03/hw03_hard.py
@@ -12,18 +12,3 @@
 # в) У пользователя надо спросить номер команды
 # г) Программа не должна завершаться пока не введется команда Выйти
 # д) Проверять на ввод ошибочных данных, там где они могут появиться
-
-
-class Menu:
-    def AddItem(self,item):
-        class Item:
-            def __init__(self,text,ToDoNext):
-                self.text=text
-                ??????????????
-        self.item.append(Item())
-    def Show():
-        for i in range(len(self.item)):
-            print(str(i+1)+") "+str(self.item[i])+"\n")
-        print("0) Back\n")
-        option=int(input())
-        self.item[option].????????????
